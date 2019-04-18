ef74bf6c096d4982cdb91dbdefcf44908bf9cec9
xiaojie <wang1418075087@qq.com>
2019-04-18 09:40:04 +0800

    新增项目截图

--- honey_shop/settings.py
+++ honey_shop/settings.py
@@ -77,23 +77,23 @@ WSGI_APPLICATION = 'honey_shop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': '数据库名字',
-        'USER': '数据库用户名',
-        'PASSWORD': '数据库密码',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': '数据库名字',
+#         'USER': '数据库用户名',
+#         'PASSWORD': '数据库密码',
+#         'HOST': '127.0.0.1',
+#         'PORT': '3306',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
